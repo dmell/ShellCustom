64257026b4a0f0fa7241ba570b3c1633430540cd
dmell <damiano.melotti@studenti.unitn.it>
2018-05-10 11:09:38 +0200

    Added exit function to header file

--- shellutil.h
+++ shellutil.h
@@ -29,6 +29,7 @@ char * substring (char * src, int first, int last);
 char** parseCommand (char * cmd, int * cmds);
 /* execute the commands */
 void run (char * cmd, char * outfile, char * errfile, int * fd, int codeFlag, int bufLenght, int logfileLenght);
-
+/* custom exit function */
+void exit(int code);
 
 #endif
